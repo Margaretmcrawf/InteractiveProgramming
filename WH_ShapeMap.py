e201ad5772b48329137d6b88b58556cf7b95e86b
Lauren Gulland <lauren.gulland@students.olin.edu>
2016-03-09 22:45:22 -0500

    Draws rooms in correct location.

--- WH_ShapeMap.py
+++ WH_ShapeMap.py
@@ -12,13 +12,12 @@ import csv
 #currently outputs to an html file which has 3 tabs - WH1, WH2, and WH3
 #rooms are currently blue squares, have hover functionality with correct labels but no data
 #Also have buttons that are currently nonfunctional but are planned to be able to display different color coordinations for each room.
-#Initializes and draws all rooms from a .csv file with data from survey sent to first years.
+#Initializes and draws all rooms in correct places from a .csv file with data from survey sent to first years.
 
 #NEXT STEPS:
 #Get more room data from people (keep collecting survey responses)
-#Draw rooms in correct places
 #Make import for roommate names work correctly
-#Make hover display room number and inhabitant names instead of '???'
+#Make hover display given room number and inhabitant names instead of '???'
 #Make buttons functional
 	#figure out how to make buttons active
 	#make metric for color changing and coordination
@@ -43,34 +42,124 @@ class Room(object):
 	def __str__(self):
 		return 'Room Number: %s / Current Inhabitants: %s / Gender: %s / Average Bedtime: %s' %(self.roomNum, self.roommates, self.gender, self.bedtime)
 
-	def drawRoom(self):
+	def findCoordinates(self): #defines positions for rooms
+		coordinates = [1,0,0,1] #default
+		mod = int(self.roomNum)%100 #can do all floors at once
+		if mod<18:
+			if mod%2==1:
+				coordinates[0] = 13
+				coordinates[1] = 12
+			else:
+				coordinates[0] = 11
+				coordinates[1] = 10
+
+			if mod==1 or mod==2:
+				coordinates[2] = 7 
+				coordinates[3] = 8
+			elif mod==3 or mod==4:
+				coordinates[2] = 8
+				coordinates[3] = 9
+			elif mod==5 or mod==6:
+				coordinates[2] = 9
+				coordinates[3] = 10
+			elif mod==7:
+				coordinates[2] = 10
+				coordinates[3] = 11
+			elif mod==9 or mod==10:
+				if int(self.roomNum)==109 or int(self.roomNum)== 110: #special case: Nick Tatar
+					coordinates[2] = 0
+					coordinates[3] = 4
+				else:
+					coordinates[2] = 3
+					coordinates[3] = 4
+			elif mod==11 or mod==12:
+				coordinates[2] = 3
+				coordinates[3] = 2
+			elif mod==13 or mod==14:
+				coordinates[2] = 2
+				coordinates[3] = 1
+			elif mod==15:
+				coordinates[2] = 0
+				coordinates[3] = 1
+		elif mod>=18:
+			if mod%2==1:
+				coordinates[2] = 6
+				coordinates[3] = 7
+			else:
+				coordinates[2] = 4
+				coordinates[3] = 5
+			if mod==18:
+				coordinates[0] = 9
+				coordinates[1] = 8
+			elif mod==20:
+				coordinates[0] = 8
+				coordinates[1] = 7
+			elif mod==23 or mod==24:
+				coordinates[0] = 6
+				coordinates[1] = 5
+			elif mod==25 or mod==26:
+				coordinates[0] = 5
+				coordinates[1] = 4
+			elif mod==27 or mod==28:
+				coordinates[0] = 4
+				coordinates[1] = 3
+			elif mod==29 or mod==30:
+				coordinates[0] = 3
+				coordinates[1] = 2
+			elif mod==31:
+				coordinates[0] = 2
+				coordinates[1] = 1
+		return coordinates
+
+	def drawRoom(self): #Now with Olin colors! heyyyyyyyyyooooooo
+		if self.gender == 'N/A':
+			color='grey'
+			alpha = .3
+		else: 
+			color='navy'
+			alpha = 1.0
 		if int(self.roomNum)<200:
 			#draw on first floor
 			rooms1 = p1.quad(top=self.top,bottom=self.bottom, left=self.left, right=self.right,
 								nonselection_fill_alpha=0.01,
-								nonselection_fill_color=None,
-								nonselection_line_color="firebrick",
+								fill_color=color,
+								alpha=alpha,
+								line_color=color,
+								hover_alpha = .7,
+								hover_fill_color=color,
+								nonselection_fill_color=color,
+								nonselection_line_color=color,
 								nonselection_line_alpha=1.0,
-								selection_color="blue",
+								selection_color=color,
 								selection_fill_alpha=.5)
 				
 		elif int(self.roomNum)<300:
 			#draw on second floor
 			rooms2 = p2.quad(top=self.top,bottom=self.bottom, left=self.left, right=self.right,
 								nonselection_fill_alpha=0.01,
-								nonselection_fill_color=None,
-								nonselection_line_color="firebrick",
+								fill_color=color,
+								alpha=alpha,
+								line_color=color,
+								hover_fill_color=color,
+								hover_alpha = .7,
+								nonselection_fill_color=color,
+								nonselection_line_color=color,
 								nonselection_line_alpha=1.0,
-								selection_color="blue",
+								selection_color=color,
 								selection_fill_alpha=.5)
 		else:
 			#draw on third floor
 			rooms3 = p3.quad(top=self.top,bottom=self.bottom, left=self.left, right=self.right,
 								nonselection_fill_alpha=0.01,
-								nonselection_fill_color=None,
-								nonselection_line_color="firebrick",
+								fill_color=color,
+								alpha=alpha,
+								line_color=color,
+								hover_alpha = .7,
+								hover_fill_color=color,
+								nonselection_fill_color=color,
+								nonselection_line_color=color,
 								nonselection_line_alpha=1.0,
-								selection_color="blue",
+								selection_color=color,
 								selection_fill_alpha=.5)
 
 
@@ -110,10 +199,11 @@ with open('WH_Freshmen.csv') as csvfile:
 		roomA.bedtime = row[3]
 		roomA.lightsleep = row[4]
 		roomA.halltime = row[5]
-		roomA.top = 13
-		roomA.bottom = 12
-		roomA.left = 0
-		roomA.right = 1
+		coordinates = roomA.findCoordinates()
+		roomA.top = coordinates[0]
+		roomA.bottom = coordinates[1]
+		roomA.left = coordinates[2]
+		roomA.right = coordinates[3]
 		roomA.drawRoom()
 		#roomList.append(roomA) #creates list of all Room objects
 
